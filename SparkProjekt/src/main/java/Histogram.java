d4bcf15e5e835f85482f63c867f6573b0a9e8cb7
Vassilios Katsaros <vassilios.katsaros@hotmail.com>
2025-09-09 21:01:12 +0200

    ok

--- SparkProjekt/src/main/java/Histogram.java
+++ SparkProjekt/src/main/java/Histogram.java
@@ -1,6 +1,7 @@
 
 // dieses Programm soll ein Histogram aus einer bestimmten Tabellenspalte einer Exceldatei
 // erstellen und abbilden
+// https://www.daten-bw.de/web/guest/suchen/-/details/strassenverkehrszahlungen-stadt-ravensburg
 
 public class Histogram {
 
